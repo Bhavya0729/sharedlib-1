aa78361bf7ea9ca88d04edf2284b31d394f879a6
Bhavya0729 <rbhavya672@gmail.com>
2025-08-25 14:15:05 +0530

    Update cicddy.groovy

--- vars/cicddy.groovy
+++ vars/cicddy.groovy
@@ -19,3 +19,9 @@ sh """
     java -cp /var/lib/jenkins/workspace/${jobname} Helloworld > /var/lib/jenkins/workspace/${jobname}/test_output.txt
 """
 }
+def release(jobname,ip)
+{
+ sh """
+scp /var/lib/jenkins/workspace/${jobname}/Helloworld.java ubuntu@${ip}:/home/ubuntu/ten
+"""
+}
